68e5c0b03946da8dc57b79e0ebb9f27a4b03c9f5
Alef Castro <alef.castro2007@hotmail.com>
2026-05-10 23:29:07 -0300

    Update ex011.py

--- exercicio/ex011.py
+++ exercicio/ex011.py
@@ -2,6 +2,6 @@ larg = float(input('Qual a largura da sua parede?'))
 alt = float(input('Qual a altura da sua parede?'))
 area = alt * larg
 litro = area / 2
-print('A dimensão é {}x{} e a area é {}m²\n'
+print('A dimensão é {}x{} e a area é {:.2}m²\n'
       'Para pintar essa parede precisa de {}L de tinta'.format(larg, alt, area,litro))
 
