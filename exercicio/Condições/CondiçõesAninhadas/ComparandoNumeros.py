1c73a5e15fcef31192d539fc81ba5d4a8b94e299
Alef Castro <alef.castro2007@hotmail.com>
2026-09-11 17:23:03 -0300

    Create ComparandoNumeros.py

---
+++ exercicio/Condições/CondiçõesAninhadas/ComparandoNumeros.py
@@ -0,0 +1,12 @@
+#Programa que leia dois numeros inteiros e compare-os qual deles é maior ou menor
+#Alef Castro
+#11/09/2026
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+if a > b:
+    print("O primeiro valor é maior!")
+elif b > a:
+    print("O segundo valor é maior!")
+else:
+    print("Não existe maior, os dois são iguais!")
