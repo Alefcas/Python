13cd1da1072725cc65b79da7ccbe56ec3250a628
Alef Castro <alef.castro2007@hotmail.com>
2026-09-16 22:16:31 -0300

    Update ConversorBaseNumerica.py

--- exercicio/Condições/CondiçõesAninhadas/ConversorBaseNumerica.py
+++ exercicio/Condições/CondiçõesAninhadas/ConversorBaseNumerica.py
@@ -2,6 +2,9 @@
 #seguindo a escolha do usuario.
 #Alef Castro
 #16/09/2026
+print("\033[31m=\033[m"*30)
+print("CONVERSOR DE BASES NUMERICAS")
+print("\033[31m=\033[m"*30)
 num = int(input("Qual o numero que deseja converter: "))
 print("Em qual base deseja converter")
 print("[1] Binário\n"
@@ -9,10 +12,13 @@ print("[1] Binário\n"
       "[3] Hexadecimal")
 opc = int(input("Sua opção: "))
 if opc == 1:
-    print("{} na base Binario é exibido como: {}".format(num, bin(num)[2:]))
+    print("{} na base Binario é exibido como: \033[35m{}".format(num, bin(num)[2:]))
+                                                #[2:]- tira o indicador da base no print!
 elif opc == 2:
-    print("{} na base Octal é exibido como: {}". format(num,oct(num)[2:]))
+    print("{} na base Octal é exibido como: \033[35m{}". format(num,oct(num)[2:]))
+                                                # [2:]- tira o indicador da base no print!
 elif opc == 3:
-    print("{} na base Hexadecimal é exibifo como: {}".format(num, hex(num)[2:]))
+    print("{} na base Hexadecimal é exibifo como: \033[35m{}".format(num, hex(num)[2:]))
+                                                # [2:]- tira o indicador da base no print!
 else:
-    print("Opção invalida!")
+    print("\033[31m!!!Opção invalida!!!\033[m")
