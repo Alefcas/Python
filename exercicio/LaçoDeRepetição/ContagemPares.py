271565df904d6965dd669e2f166cb7c27efa4c0f
Alef Castro <alef.castro2007@hotmail.com>
2026-09-20 14:38:33 -0300

    Update ContagemPares.py

--- exercicio/LaçoDeRepetição/ContagemPares.py
+++ exercicio/LaçoDeRepetição/ContagemPares.py
@@ -1,7 +1,7 @@
 #Programa que mostre na tela todos os númeos pares entre 1 e 50
 #Alef Castro
 #20/09/2026
-print()
+print("Contagem de todos os pares entre 1 e 50.")
 for i in range(1,50):
     if i %2 == 0:
         print(i)
