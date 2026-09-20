0de5e8ccd6ebccab1b81571eb3cfafeec95adbfd
Alef Castro <alef.castro2007@hotmail.com>
2026-09-20 14:38:30 -0300

    Update FogosArtificio.py

--- exercicio/LaçoDeRepetição/FogosArtificio.py
+++ exercicio/LaçoDeRepetição/FogosArtificio.py
@@ -2,8 +2,9 @@
 #Alef Castro
 #20/09/2026
 from time import sleep
+print("\033[34mCONTAGEM REGRESSIVA\033[m")
 seg = int(input("quantos segundos para estourar o fogo de artificio: "))
 for cont in range(seg, 0, -1):
     print(cont)
     sleep(1)
-print("BOOOOMM!!!")
+print("\033[31mBOOOOMM!!!")
