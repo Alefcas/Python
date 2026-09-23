dbc722c991ca1f1ee1c47005b7744ef398425069
Alef Castro <alef.castro2007@hotmail.com>
2026-09-23 10:46:04 -0300

    Create ImparesMutiplosTres.py

---
+++ exercicio/LaçoDeRepetição/ImparesMutiplosTres.py
@@ -0,0 +1,11 @@
+#Programa que calcule a soma entre todos os números Impares que são múltiplos de três e que se
+#encontram no intervalo de 1 até 500.
+#Alef Castro
+#23/09/2026
+soma = 0
+cont = 0
+for i in range(1,501,2):
+    if i % 3 == 0:
+        soma += i
+        cont += 1
+print("A soma de todos os {} valores solicitados é {}".format(cont, soma))
